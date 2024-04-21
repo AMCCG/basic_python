7c2147d3c85586aedc7abe319b3b74709d72a623
apisit.a <amornchanchaigul@gmail.com>
2024-04-21 22:04:43 +0700

    add python hacker rank

--- hello_python.py
+++ hello_python.py
@@ -1,1 +1,12 @@
-print("Hello Python")
+import this
+
+# Pythonic Code Style
+print("Pythonic Code Style")
+print("Clean")
+print("Simple")
+print("Beautiful")
+print("Explicit")
+print("Readable")
+
+# Python
+print("Python was developed by Guido van Rossum")
